0c5297db4bfbe4903e9b79440ceef5d57fae0c66
Heverton Monteiro <heverton.r.s.monteiro@gmail.com>
2026-02-05 16:58:02 -0300

    atividades
    desafio7
    desafio8
    desafio9

---
+++ desafio7/atividade1.py
@@ -0,0 +1,18 @@
+print("Bem vindo a tupla! ")
+tupla1 = (1,2)
+tupla2 = (3,4)
+soma = tupla1+tupla2
+quantidade = len(soma)
+maior = max(soma)
+menor = min(soma)
+numero = int(input("Digite um numero: "))
+if numero in soma:
+    print("Existe esse numero na tupla! ")
+else:
+    print("Não existe esse numero na tupla! ")
+
+
+
+
+
+
