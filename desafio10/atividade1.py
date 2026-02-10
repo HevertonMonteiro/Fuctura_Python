f749e38c212199c87cf0f300da25f97fae0d78f7
Heverton Monteiro <heverton.r.s.monteiro@gmail.com>
2026-02-10 19:51:53 -0300

    atualização
    Coloquei a verificacao dentro da função

--- desafio10/atividade1.py
+++ desafio10/atividade1.py
@@ -1,18 +1,22 @@
 def verificar_media(nota1, nota2, nota3):
-    return (nota1 + nota2 + nota3) / 3
+    media = (nota1 + nota2 + nota3) / 3
+
+    if media < 4:
+        print('Reprovado!')
+    elif media >= 7:
+        print('Aprovado!')
+    else:
+        print('Recuperação!')
+
 print('--Insira suas notas--')
 nota1 = float(input('Nota 1: '))
 nota2 = float(input('Nota 2: '))
 nota3 = float(input('Nota 3: '))
 
-media = verificar_media(nota1, nota2, nota3)
+resultado = verificar_media(nota1, nota2, nota3)
 
-if media < 4:
-    print('Reprovado!')
-elif media >= 7:
-    print('Aprovado!')
-else:
-    print('Recuperação!')
+print(f'Média final: {resultado:.2f}')
 
 
-print(f'Média final: {media:.2f}')
+
+
--- desafio11/atividade1.py
+++ desafio11/atividade1.py
@@ -1,25 +1,29 @@
 def verificar_media(nota1, nota2):
-    return(nota1+nota2)/2
+    media = (nota1 + nota2) / 2
+
+    if media < 4:
+        print('Reprovado!')
+    elif media < 7:
+        print('Recuperação!')
+    else:
+        print('Aprovado!')
+
+    return media
+
 
 quantidade_alunos = int(input('Quantos alunos tem nessa sala? '))
 media_geral = 0
-for q in range (quantidade_alunos):
 
-    print(f'\nInforme suas notas: {q+1} ')
+for q in range(quantidade_alunos):
+    print(f'\nInforme as notas do aluno {q + 1}')
     nota1 = float(input('Digite a nota 1: '))
-    nota2 = float(input('Digite a nota 1: '))
+    nota2 = float(input('Digite a nota 2: '))
 
     media = verificar_media(nota1, nota2)
-    media_geral += media / quantidade_alunos
+    media_geral += media
 
-    if media < 4:
-        print('Reprovado! ')
-    elif media == 7:
-        print('Aprovado por Media! ')
-    elif media > 7:
-        print('Aprovado! ')
-    else:
-        print('Recuperação! ')
+
+media_geral /= quantidade_alunos
 
 print('\n============================')
-print(f'A media geral da sala é {media_geral: .2f}')
+print(f'A média geral da sala é {media_geral:.2f}')
